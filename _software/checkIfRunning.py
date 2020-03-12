9609f3fcbebc2fc350053529d079ab156c5bc2dc
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2020-03-12 09:54:23 -0700

    Updated Indentation

--- _software/checkIfRunning.py
+++ _software/checkIfRunning.py
@@ -1,25 +1,14 @@
 import os,sys
 
-
 def checkProcess(processName):
-
     output = os.popen("pgrep -af python").read()
-
-
     if output.find(processName) != -1:
-        #print("Process Running")
-        #print("Running")
         return 1
     else:
-        #print("Process NOT Running")
-        #print("Stopped")
         return 0
-
+    
 processName1 = "sudo python main.py"
 processName2 = "python /home/pi/Documents/DataLogger/_software/main.py"
-#checkProcess(processName)
-
-
 check1 = checkProcess(processName1)
 check2 = checkProcess(processName2)
 
