f99edab9e8f13436fd6b45f935d32946a866f92d
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2020-03-12 09:48:12 -0700

    Updated Indentation.

--- _software/adc_test.py
+++ _software/adc_test.py
@@ -11,29 +11,16 @@ current = 100/1000000.0 #current source, 100microamps
 offset = 0#-3.40
 
 def convert_ADC(adcReading):
-
-	print adcReading
-	#adcReading = adcReading.replace('\n', '') #take out newline
-	#adc_int = int(adcReading, 16)
-	#print adc_int
-	
-	inputShifted = int(binascii.hexlify(adcReading), 16) >> 6
-	print ("Input shifted:" + str(inputShifted))
-	maskedOutput = 0xFFFFFF & inputShifted
-    #6 bit shift, 24 bit mask?
-	print(maskedOutput)
-
-
-	voltageAcrossRTD = float(float(maskedOutput) / float(16777216)) #fpr 24 bit adc
-	toVoltage = (voltageAcrossRTD * FS) / opAmpGain
-
-    #print("FS: " + str(FS))
-	print(voltageAcrossRTD)
-	print("ADC Voltage: " + str(toVoltage))
-
-
-	RTDResistance = toVoltage / current  + offset
-	print ("RTD Resistance: " + str(RTDResistance))
-	
-#output = check_output(["i2cget", "-y", "1", "0x16", "0x00", "w"])
+    print(adcReading)
+    inputShifted = int(binascii.hexlify(adcReading), 16) >> 6
+    print ("Input shifted:" + str(inputShifted))
+    maskedOutput = 0xFFFFFF & inputShifted
+    print(maskedOutput)
+    voltageAcrossRTD = float(float(maskedOutput) / float(16777216)) #fpr 24 bit adc
+    toVoltage = (voltageAcrossRTD * FS) / opAmpGain
+    print(voltageAcrossRTD)
+    print("ADC Voltage: " + str(toVoltage))
+    RTDResistance = toVoltage / current  + offset
+    print ("RTD Resistance: " + str(RTDResistance))
+##-----------------------------------------------------Main
 convert_ADC(binascii.unhexlify("ab8ed4bc"))
