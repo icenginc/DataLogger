ea474e804378c095c11dc04aebdbc4564191f5f9
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2020-03-12 11:18:17 -0700

    Updated Indentation

--- _software/outputCSV.py
+++ _software/outputCSV.py
@@ -1,23 +1,14 @@
 import sqlite3
 
 def getData(systemNum, date):
-
     conn = sqlite3.connect('../_database/temperatures.db')
-
     statement = "SELECT * FROM dataTable WHERE system = '" + systemNum + "' AND strftime('%Y-%m-%d',date) = date('" + date + "') LIMIT 10"
-
     print(statement)
-
     cursor = conn.cursor()
     cursor.execute(statement)
-
     rows = cursor.fetchall()
-
     for item in rows:
-            print(item)
-
-
+        print(item)
     conn.close()
-
 
 getData("RPI_001", "2017-06-02")
