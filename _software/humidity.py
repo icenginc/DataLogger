68b90f6129d55e282a5a8a9524fee17ce654236c
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2020-03-12 15:16:22 -0700

    Update humidity.py

--- _software/humidity.py
+++ _software/humidity.py
@@ -26,7 +26,7 @@ def strip_output(input):
     #input = input.replace("Temperature: ", "")
     input =input[13:-3]
     return input
-	#see link for below 2 equations : https://www.1728.org/relhum.htm
+    #see link for below 2 equations : https://www.1728.org/relhum.htm
 
 def calculate_const(input):
     numerator = float(input)*17.502
