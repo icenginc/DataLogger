c256431a8b46fecb6217f1a6636bbcb65dbdd52d
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2020-03-12 09:23:36 -0700

    Updated indentation.

--- _software/LCDLibrary.py
+++ _software/LCDLibrary.py
@@ -1,4 +1,3 @@
-
 import time
 import pigpio
 
@@ -11,12 +10,11 @@ def initLCD():
 
     handle = pi.i2c_open(1, LCD_Address)
     time.sleep(.01)
-    
+
     pi.i2c_write_device(handle, [0x00,0x38,0x39,0x14,0x78,0x5E,0x6D,0x0c,0x01,0x06])
     time.sleep(0.01)
 
     pi.i2c_close(handle)
-
 
 def writeText(text):
 
@@ -24,10 +22,8 @@ def writeText(text):
     time.sleep(.01)
 
     pi.i2c_write_byte(handle, 0x40)
-        
+
     pi.i2c_write_device(handle, text)
     time.sleep(.01)
-    
+
     pi.i2c_close(handle)
-
-
