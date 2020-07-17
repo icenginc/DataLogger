1932946da8386c59007910cdf1b086943c6e81b8
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2020-07-17 13:47:14 -0700

    Update getSystemStatus.php

--- _website/getSystemStatus.php
+++ _website/getSystemStatus.php
@@ -4,7 +4,7 @@
 	function runScript2()
 	{
 		$scriptName = "checkSystemStatus.py";
-		$command = escapeshellcmd("/home/pi/Documents/DataLogger/_software/".$scriptName);
+		$command = escapeshellcmd("/home/pi/Documents/DataLogger/_software/website/".$scriptName);
 		//echo "/home/pi/Documents/DataLogger/_software/".$scriptName;
 		$command = "python ".$command;
 		
