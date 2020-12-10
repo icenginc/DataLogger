be450313bcf23df01f487de12e03b080196a7fc1
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2020-12-10 10:36:35 -0800

    Update checkandreboot.py

--- _software/checkandreboot.py
+++ _software/checkandreboot.py
@@ -2,8 +2,6 @@ from subprocess import check_output
 import os
 
 
-#----WARNING: SCRIPT MAY CALL MULTIPLE INSTANCES.
-
 def main():
     check1=check_output(["python", "/home/pi/Documents/DataLogger/_software/website/checkIfRunning.py"])
     if(check1 == "Stopped\n"):
