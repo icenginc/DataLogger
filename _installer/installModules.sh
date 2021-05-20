d21ff5c42919a456621ff5c2fc785f05cffb0262
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2021-05-20 11:43:14 -0700

    Update installModules.sh

--- _installer/installModules.sh
+++ _installer/installModules.sh
@@ -2,17 +2,15 @@
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
 
-sudo apt-get install i2c-tools
-
-sudo wget https://github.com/joan2937/pigpio/archive/master.zip
-sudo unzip master.zip
-cd pigpio-master
-sudo make
-sudo make install
-sudo pigpiod start
-
+sudo apt-get install i2c-tools -y
 sudo apt-get install apache2 -y
 sudo apt-get install php-sqlite3 -y
 sudo apt-get install php -y
 
-sudo apt-get install vim -y
+sudo apt-get install vim
+
+wget https://github.com/joan2937/pigpio/archive/master.zip
+unzip master.zip
+cd pigpio-master
+make
+sudo make install
