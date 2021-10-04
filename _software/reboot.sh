7999d081c0386795eee612d38768cf0d25c79f06
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2021-10-04 16:25:31 -0700

    Update reboot.sh

--- _software/reboot.sh
+++ _software/reboot.sh
@@ -2,6 +2,6 @@
 
 echo "Killing Python..."
 sudo killall python
-echo "Starting Back Up..."
-sudo python /home/pi/Documents/DataLogger/_software/main.py
+#echo "Starting Back Up..."
+#sudo python /home/pi/Documents/DataLogger/_software/main.py
 #exit(0)
