11ee75ae31f40d54af316a33e9422db39396db80
Chrishn10 <54902571+Chrishn10@users.noreply.github.com>
2022-01-11 13:07:18 -0800

    Update checkandreboot.py

--- _software/checkandreboot.py
+++ _software/checkandreboot.py
@@ -1,10 +1,15 @@
 from subprocess import check_output
 import os
-
+import postdata
+import uploadCSV
+import time
 
 def main():
     check1=check_output(["python", "/home/pi/Documents/DataLogger/_software/website/checkIfRunning.py"])
     if(check1 == "Stopped\n"):
+        dictionaryData=uploadCSV.getAllDictionaries()
+        postdata.updatestop(dictionaryData['RP_Name'])
+        time.sleep(10)
         print("Rebooting process...")
         os.system('bash /home/pi/Documents/DataLogger/_software/reboot.sh')
     else:
